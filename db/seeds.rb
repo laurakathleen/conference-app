1c43d69fd997b03ff1ee72c19d7bbb78c230f62a
laurakathleen <laurakwalley@gmail.com>
2016-09-29 11:00:21 -0700

    seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+p "Hello from seeds.rb"
+
+#Delete all speakers:
+Speaker.delete_all
+#Create an array of speaker data:
+Speaker.create speakers_data = [
+    {:first=>"Cory", :last=>"Fauver", :email=>"cory@example.com"},
+    {:first=>"Brianna", :last=>"Veenstra", :email=>"brianna@example.com"},
+    {:first=>"Matt", :last=>"Laguardia", :email=>"matt@example.com"},
+    {:first=>"Ilias", :last=>"Tsangaris", :email=>"ilias@example.com"}
+]
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
